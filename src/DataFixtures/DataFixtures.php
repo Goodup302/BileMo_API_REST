46833e1f4dba93023664e7c097ca6e181ba7feee
Goodup302 <j.f0471430704@gmail.com>
2019-05-27 11:26:58 +0200

    update fixtures name

--- src/DataFixtures/DataFixtures.php
+++ src/DataFixtures/DataFixtures.php
@@ -8,7 +8,7 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ProductFixtures extends Fixture
+class DataFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
